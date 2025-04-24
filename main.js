54f47e4dea9d0a3dade9223c707df6cb22b84b98
IsmoilovHusniddin-coder <ismoilovh504@gmail.com>
2025-04-24 22:19:48 +0500

    finished

---
+++ main.js
@@ -0,0 +1,74 @@
+//Alisher akani sayohati
+
+// var a = 500;
+// var b = 250;
+// var c = 120;
+// var result1 = a * 11000;
+// var result2 = b * 11000;
+// var result3 = c * 12354;
+// var resultoveral = result1 + result2 + result3;
+// var sum = prompt("Hisobingizni kiriting");
+// if (sum >= resultoveral) {
+//   console.log("Oq yo'l, Alisher ");
+// } else if (sum < resultoveral) {
+//   console.log("Alisher ozgina sabr qilishi kerak bo'lar ekan");
+// }
+
+//ko'p xonali sonlar
+
+// var number = prompt("Sonni kiriting");
+// if (number >= 0 && number <= 9) {
+//   console.log("Bu bir xonali son");
+// } else if (number >= 10 && number <= 99) {
+//   console.log("Bu ikki xonali son");
+// } else if (number >= 100 && number <= 999) {
+//   console.log("Bu uch xonali son");
+// } else if (number >= 1000 && number <= 9999) {
+//   console.log("Bu to'rt xonali son");
+// }
+
+//Sonning juft yoki toqligi
+
+// var number = prompt("Sonni kiriting");
+// var result1 = number % 2;
+// if (result1 == 0) {
+//   console.log("Bu son juft");
+// } else if (result1 == 1) {
+//   console.log("Bu son toq");
+// }
+
+//ko'p xonali sonlar va sonning juft yoki toqligi
+
+// var number = prompt("Sonni kiriting");
+// var result1 = number % 2;
+// if (result1 == 0 && number >= 0 && number <= 9) {
+//   console.log("Bu son juft va bir xonali");
+// } else if (result1 == 0 && number >= 10 && number <= 99) {
+//   console.log("Bu son juft va ikki xonali");
+// } else if (result1 == 0 && number >= 100 && number <= 999) {
+//   console.log("Bu son juft va uch xonali");
+// } else if (result1 == 0 && number >= 1000 && number <= 9999) {
+//   console.log("Bu son juft va to'rt xonali");
+// } else if (result1 == 1 && number >= 0 && number <= 9) {
+//   console.log("Bu son toq va bir xonali");
+// } else if (result1 == 1 && number >= 10 && number <= 99) {
+//   console.log("Bu son toq va ikki xonali");
+// } else if (result1 == 1 && number >= 100 && number <= 999) {
+//   console.log("Bu son toq va uch xonali");
+// } else if (result1 == 1 && number >= 1000 && number <= 9999) {
+//   console.log("Bu son toq va to'rt xonali");
+// }
+
+// 3 ta son kiritiladi
+// var a = Number(prompt("1-sonni kiriting"));
+// var b = Number(prompt("2-sonni kiriting"));
+// var c = Number(prompt("3-sonni kiriting"));
+
+// var numbers = [a, b, c];
+// numbers.sort(function (x, y) {
+//   return x - y;
+// });
+
+// var result = numbers.join("");
+
+// console.log("Natija:", result);
